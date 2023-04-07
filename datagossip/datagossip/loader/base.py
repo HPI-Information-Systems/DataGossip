d20ca1a0c7739c4f019457dfe5c35b33d396f278
Phillip W <info@pwenig.de>
2023-04-07 14:38:38 +0200

    tests

--- datagossip/datagossip/loader/base.py
+++ datagossip/datagossip/loader/base.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from typing import List, Iterator, Type
+import torch.multiprocessing as mp
 
 from .foreign import ForeignDataIterator
 from ..process import DataGossipProcess
@@ -14,8 +15,8 @@ class DataGossipLoader:
                  data_shape: List[int],
                  args,
                  foreign_data_loader: Type[ForeignDataIterator] = ForeignDataIterator):
+        mp.set_start_method('spawn')
         self.local_data_loader = data_loader
-
         # foreign data
         n_neighbors = args.size # group size
         self.foreign_data = torch.zeros(n_neighbors - 1, args.k, *data_shape) - 1
