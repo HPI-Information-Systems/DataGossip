17211bbbe653059103260e3630974f511ec07efb
Phillip W <info@pwenig.de>
2023-04-08 12:08:15 +0200

    tests

--- datagossip/utils/__init__.py
+++ datagossip/utils/__init__.py
@@ -2,7 +2,7 @@ import torch.multiprocessing as mp
 
 
 def set_start_method(method="spawn"):
-    start_method = mp.get_start_method()
-    print(f"start method: {start_method}")
-    if start_method != method:
+    try:
         mp.set_start_method(method)
+    except RuntimeError:
+        pass
